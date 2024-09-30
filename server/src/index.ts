ab9444efca4aa9bd113542b274e1c03fb984567f
urdadx <nerdyshinobi12@gmail.com>
2024-09-30 23:01:10 +0000

    added biomejs formatter

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,11 @@
-import express, { Request, Response } from "express";
+import express, { type Request, type Response } from "express";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send(`<span>server is working</span>`);
+	res.send("<span>server is working</span>");
 });
 
 app.listen(process.env.PORT || 8000, () => {
-  console.log(`listening on port ${process.env.PORT || 8000}`);
+	console.log(`listening on port ${process.env.PORT || 8000}`);
 });
