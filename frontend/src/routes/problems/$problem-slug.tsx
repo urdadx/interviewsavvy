f7cc23c84103b449942b617f142ae633c0921f19
urdadx <nerdyshinobi12@gmail.com>
2024-10-06 11:53:58 +0000

    fixed prompt form alignment

--- frontend/src/routes/problems/$problem-slug.tsx
+++ frontend/src/routes/problems/$problem-slug.tsx
@@ -3,14 +3,14 @@ import Panes from "../../components/playground/panes";
 import { Navbar } from "../../components/shared/navbar";
 
 export const Route = createFileRoute("/problems/$problem-slug")({
-	component: Problem,
+  component: Problem,
 });
 
 function Problem() {
-	return (
-		<div className="h-screen flex flex-col bg-surface-50">
-			<Navbar />
-			<Panes />
-		</div>
-	);
+  return (
+    <div className="h-screen w-screen flex flex-col bg-surface-50">
+      <Navbar />
+      <Panes />
+    </div>
+  );
 }
