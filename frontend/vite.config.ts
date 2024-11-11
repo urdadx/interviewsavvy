76c24907f2021a9cd4b3b203c99e7c602d352933
urdadx <70736338+urdadx@users.noreply.github.com>
2024-11-11 21:27:13 +0000

    Problems table (#18)
    * better textarea
    
    * first pull from server - auth
    
    * made textarea better for chat
    
    * added timer, chat message and waitlist page
    
    * removed .env.local from root
    
    * cleanedup ui and QOL improvements
    
    * added waitlist logic
    
    * minor changes
    
    * added sidebar
    
    * fixed layout issues

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [TanStackRouterVite({ routeToken: "layout" }), viteReact()],
+	plugins: [TanStackRouterVite(), viteReact()],
 	resolve: {
 		alias: {
 			"@shared": "../shared/*",
